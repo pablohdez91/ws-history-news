9bfb7984b2c914cf51c69dcdf63d2423d6338a13
Pablo Hernandez <pablohdez91@gmail.com>
2022-02-24 13:38:22 -0600

    first try of request with urllib3

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-msg = "Hello world!"
-print(msg)
+import urllib3
+
+http = urllib3.PoolManager()
+url = "https://busquedas.gruporeforma.com/reforma/BusquedasComs.aspx"
+r = http.request('GET', url)
+print(r.data)
+
